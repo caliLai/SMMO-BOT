93221e91ef90987e195e0fde2081b14097e8be26
caliLai <calilai2018@gmail.com>
2020-12-28 03:49:00 -0800

    slightly new changes

--- main.js
+++ main.js
@@ -13,44 +13,44 @@ const axios = require("axios"),
 Discord = require("discord.js"),
 smmo = require("./SMMO_handler.js");
 
-//------API STUFF-----//
-const SMMO_apiKey = "vMoVvw5YfyariOUfmRSk2mhczkfHY9ZbWsFucSB2muSQ2GWlTtRdOeGDiwxhvf7k6o4Of9O0vRTi7aww",
-    SMMO_wbURL = "https://api.simple-mmo.com/v1/worldboss/all";
-
 //-----IDK MAN-----//
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const DC_TOKEN = process.env.DC_TOKEN;
+
+//------API STUFF-----//
+const SMMO_apiKey = process.env.SMMO_TOKEN,
+    SMMO_wbURL = "https://api.simple-mmo.com/v1/worldboss/all";
 
 //-----OH BOY, FUN STUFF STARTS HERE-----//
-bot.login(TOKEN);
+bot.login(DC_TOKEN);
 
 // make a HTTP POST request to SMMO
-let fuck = axios.post(SMMO_wbURL, {api_key: SMMO_apiKey})
-    .then(res => {
-        return new Promise((resolve, reject) => {
-            let now = new Date().getTime(); // current time
-            for(boss of res.data){
-                // note that enable_time is in seconds and now is in miliseconds
-                if(boss.enable_time - (now/1000) > 7200){
-                    resolve(boss.name);
+let fuck = () => {
+    axios.post(SMMO_wbURL, {api_key: SMMO_apiKey})
+        .then(res => {
+            return new Promise((resolve, reject) => {
+                let now = new Date().getTime(); // current time
+                for(boss of res.data){
+                    // note that enable_time is in seconds and now is in miliseconds
+                    // check to see if there's a boss within the next hour
+                    if(boss.enable_time - (now/1000) > 0){
+                        resolve(boss.name);
+                    }
                 }
-            }
-            reject("no bossess attackable soon");
+                reject("no bossess attackable soon");
+            })
         })
-
-        // console.log(res.data);
-        //smmo.attackInFive(res);
-    })
-    .then(b => {
-        // okay, I honestly don't know how to not put a promise inside
-        // another promise. But hey, it's working.
-        bot.channels.fetch("791497108351615049")
-          .then(channel => channel.send(
-              `${b} will be attackable soon`
-          ))
-          .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
+        .then(b => {
+            // okay, I honestly don't know how to not put a promise inside
+            // another promise. But hey, it's working.
+            bot.channels.fetch("791497108351615049")
+              .then(channel => channel.send(
+                  `${b} will be attackable soon`
+              ))
+              .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
+}
 
 
-setInterval(fuck, 3600000);
+//setInterval(fuck, 3600000);
